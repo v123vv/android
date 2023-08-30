f30738e8c66422e7f294e2f6f08c87dac0096294
bin <bin@>
2023-08-31 00:00:14 +0800

    activity

---
+++ app/src/main/java/com/example/learn/kt/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.learn.kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.learn.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
